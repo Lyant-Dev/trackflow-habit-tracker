9d03fea55439604ac2dce5767e2921c264c4ba9c
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-04-28 17:12:39 +0700

    SVG

--- app/mainPageComponents/Navbar.tsx
+++ app/mainPageComponents/Navbar.tsx
@@ -1,9 +1,8 @@
 import React from "react"; //6.9k (gzipped: 2.7k)
 import AppIcon from "../SVG_Icons/AppIcon";
 
-function NavBar() {
+function Navbar() {
   const defaultColor = "#04d400";
-  const backgroundColorObject = { backgroundColor: defaultColor };
   return (
     <header>
       <div className=" p-8 px-20">
@@ -11,19 +10,21 @@ function NavBar() {
           <div className="text-center sm:text-left mb-7 sm:mb-0">
             {/* Icon + Name of the App */}
             {/* ----------------------- */}
-            <div className="flex gap-2 items-center sm:justify-start jsutify-center">
+            <div className="flex gap-2 items-center sm:justify-start justify-center">
               <span className="text-2xl font-light flex items-center gap-2">
                 {/* The Icon */}
-                <div style={backgroundColorObject} className="p-2 rounded-md">
+                const defaultColor = "#04d400";
+                <div
+                  className="p-2 rounded-md"
+                  style={{ backgroundColor: defaultColor }}
+                >
                   <AppIcon color="#ffffff" height="34" width="34" />
                 </div>
-                {/* The Name of the App  */}
-                <span
-                  style={{ color: " #04d400" }}
-                  className="font-bold text-mainColor"
-                >
+                <span className="font-bold" style={{ color: defaultColor }}>
                   track
                 </span>
+                {/* The Name of the App  */}
+                <span className="font-bold text-[#04d400]">track</span>
                 <span className="font-light"> Flow</span>
               </span>
             </div>
@@ -33,3 +34,5 @@ function NavBar() {
     </header>
   );
 }
+
+export default Navbar;
