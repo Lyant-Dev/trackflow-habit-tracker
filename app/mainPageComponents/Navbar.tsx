ae2b29ac93d20893a2a1189e53f564d854f61ccf
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-04-28 17:13:50 +0700

    fix navbar

--- app/mainPageComponents/Navbar.tsx
+++ app/mainPageComponents/Navbar.tsx
@@ -13,18 +13,16 @@ function Navbar() {
             <div className="flex gap-2 items-center sm:justify-start justify-center">
               <span className="text-2xl font-light flex items-center gap-2">
                 {/* The Icon */}
-                const defaultColor = "#04d400";
-                <div
-                  className="p-2 rounded-md"
-                  style={{ backgroundColor: defaultColor }}
-                >
+                <div  className="bg-[#04d400] p-2 rounded-md">
                   <AppIcon color="#ffffff" height="34" width="34" />
+                  
                 </div>
-                <span className="font-bold" style={{ color: defaultColor }}>
+                {/* The Name of the App  */}
+                <span
+                  className="font-bold text-[#04d400]"
+                >
                   track
                 </span>
-                {/* The Name of the App  */}
-                <span className="font-bold text-[#04d400]">track</span>
                 <span className="font-light"> Flow</span>
               </span>
             </div>
